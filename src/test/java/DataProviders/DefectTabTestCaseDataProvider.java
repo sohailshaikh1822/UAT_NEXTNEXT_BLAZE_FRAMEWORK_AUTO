71b08680ba3f0f0acb92dde5cbad7917f27e0036
SohailShaikhFaiyaz <sohail_sf@pursuitsoftware.com>
2025-12-01 12:53:44 +0530

    Fixed

--- src/test/java/DataProviders/DefectTabTestCaseDataProvider.java
+++ src/test/java/DataProviders/DefectTabTestCaseDataProvider.java
@@ -9,241 +9,241 @@ public class DefectTabTestCaseDataProvider {
 
     @DataProvider(name = "tc001")
     public static Object[][] getDataTC001() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc001");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc001");
     }
 
     @DataProvider(name = "tc002")
     public static Object[][] getDataTC002() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc002");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc002");
     }
 
     @DataProvider(name = "tc003")
     public static Object[][] getDataTC003() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc003");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc003");
     }
 
     @DataProvider(name = "tc004")
     public static Object[][] getDataTC004() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc004");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc004");
     }
 
     @DataProvider(name = "tc005")
     public static Object[][] getDataTC005() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc005");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc005");
     }
 
     @DataProvider(name = "tc006")
     public static Object[][] getDataTC006() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc006");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc006");
     }
 
     @DataProvider(name = "tc007")
     public static Object[][] getDataTC007() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc007");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc007");
     }
 
     @DataProvider(name = "tc008")
     public static Object[][] getDataTC008() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc008");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc008");
     }
 
     @DataProvider(name = "tc009")
     public static Object[][] getDataTC009() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc009");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc009");
     }
 
     @DataProvider(name = "tc010")
     public static Object[][] getDataTC010() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc010");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc010");
     }
 
     @DataProvider(name = "tc011")
     public static Object[][] getDataTC011() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc011");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc011");
     }
 
     @DataProvider(name = "tc012")
     public static Object[][] getDataTC012() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc012");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc012");
     }
 
     @DataProvider(name = "tc013")
     public static Object[][] getDataTC013() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc013");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc013");
     }
 
     @DataProvider(name = "tc014")
     public static Object[][] getDataTC014() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc014");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc014");
     }
 
     @DataProvider(name = "tc015")
     public static Object[][] getDataTC015() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc015");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc015");
     }
 
     @DataProvider(name = "tc016")
     public static Object[][] getDataTC016() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc016");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc016");
     }
 
     @DataProvider(name = "tc017")
     public static Object[][] getDataTC017() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc017");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc017");
     }
 
     @DataProvider(name = "tc018")
     public static Object[][] getDataTC018() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc018");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc018");
     }
 
     @DataProvider(name = "tc019")
     public static Object[][] getDataTC019() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc019");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc019");
     }
 
     @DataProvider(name = "tc020")
     public static Object[][] getDataTC020() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc020");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc020");
     }
 
     @DataProvider(name = "tc021")
     public static Object[][] getDataTC021() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc021");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc021");
     }
 
     @DataProvider(name = "tc022")
     public static Object[][] getDataTC022() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc022");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc022");
     }
 
     @DataProvider(name = "tc023")
     public static Object[][] getDataTC023() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc023");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc023");
     }
 
     @DataProvider(name = "tc024")
     public static Object[][] getDataTC024() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc024");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc024");
     }
 
     @DataProvider(name = "tc025")
     public static Object[][] getDataTC025() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc025");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc025");
     }
 
     @DataProvider(name = "tc026")
     public static Object[][] getDataTC026() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc026");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc026");
     }
 
     @DataProvider(name = "tc027")
     public static Object[][] getDataTC027() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc027");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc027");
     }
 
     @DataProvider(name = "tc028")
     public static Object[][] getDataTC028() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc028");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc028");
     }
 
     @DataProvider(name = "tc029")
     public static Object[][] getDataTC029() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc029");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc029");
     }
 
     @DataProvider(name = "tc030")
     public static Object[][] getDataTC030() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc030");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc030");
     }
 
     @DataProvider(name = "tc031")
     public static Object[][] getDataTC031() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc031");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc031");
     }
 
     @DataProvider(name = "tc032")
     public static Object[][] getDataTC032() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc032");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc032");
     }
 
     @DataProvider(name = "tc033")
     public static Object[][] getDataTC033() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc033");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc033");
     }
 
     @DataProvider(name = "tc034")
     public static Object[][] getDataTC034() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc034");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc034");
     }
 
     @DataProvider(name = "tc035")
     public static Object[][] getDataTC035() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc035");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc035");
     }
 
     @DataProvider(name = "tc036")
     public static Object[][] getDataTC036() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc036");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc036");
     }
 
     @DataProvider(name = "tc037")
     public static Object[][] getDataTC037() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc037");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc037");
     }
 
     @DataProvider(name = "tc038")
     public static Object[][] getDataTC038() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc038");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc038");
     }
 
     @DataProvider(name = "tc039")
     public static Object[][] getDataTC039() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc039");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc039");
     }
 
     @DataProvider(name = "tc040")
     public static Object[][] getDataTC040() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc040");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc040");
     }
 
     @DataProvider(name = "tc041")
     public static Object[][] getDataTC041() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc041");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc041");
     }
 
     @DataProvider(name = "tc042")
     public static Object[][] getDataTC042() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc042");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc042");
     }
 
     @DataProvider(name = "tc043")
     public static Object[][] getDataTC043() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc043");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc043");
     }
 
     @DataProvider(name = "tc044")
     public static Object[][] getDataTC044() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc044");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc044");
     }
 
     @DataProvider(name = "tc045")
     public static Object[][] getDataTC045() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc045");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc045");
     }
 
     @DataProvider(name = "tc046")
     public static Object[][] getDataTC046() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc046");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc046");
     }
 
     @DataProvider(name = "tc047")
     public static Object[][] getDataTC047() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc047");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc047");
     }
 
     @DataProvider(name = "tc048")
     public static Object[][] getDataTC048() throws IOException {
-        return ExcelDataProviders.getExcelDataForDefectTab("tc048");
+        return ExcelDataProviders.getExcelDataForDefectTestCase("tc048");
     }
 }
--- src/test/java/utils/ExcelDataProviders.java
+++ src/test/java/utils/ExcelDataProviders.java
@@ -89,4 +89,20 @@ public class ExcelDataProviders {
         }
         return data;
     }
+    public static String[][] getExcelDataForDefectTestCase(String sheetName) throws IOException {
+        String path = System.getProperty("user.dir") + "/testData/defectTabTestData/defectTestCaseTab_testData.xlsx";
+        XLUtility utility = new XLUtility(path);
+
+        int totalRows = utility.getRowCount(sheetName);
+        int totalCols = utility.getCellCount(sheetName, 1);
+
+        String[][] data = new String[totalRows][totalCols];
+
+        for (int i = 1; i <= totalRows; i++) {
+            for (int j = 0; j < totalCols; j++) {
+                data[i - 1][j] = utility.getCellData(sheetName, i, j);
+            }
+        }
+        return data;
+    }
 }
