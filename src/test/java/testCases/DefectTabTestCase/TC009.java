2301b67016274aee6dcedf2cb27c6efb494836ca
MohitAman <mohit_a@pursuitsoftware.com>
2025-12-01 15:20:28 +0530

    TC009 automated

---
+++ src/test/java/testCases/DefectTabTestCase/TC009.java
@@ -0,0 +1,62 @@
+package testCases.DefectTabTestCase;
+
+import DataProviders.DefectTabTestCaseDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.defectTab.CreateDefectPage;
+import pageObjects.defectTab.DefectLandingPage;
+import testBase.BaseClass;
+import utils.RetryAnalyzer;
+
+public class TC009 extends BaseClass {
+    @Test(dataProvider = "tc009", dataProviderClass = DefectTabTestCaseDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
+    public void Verify_dropdown_mandatory_validation_does_not_accept_placeholder(
+            String expectedUrlAfterClick,
+            String Summary,
+            String Description
+    ) throws InterruptedException {
+
+        logger.info("****** Starting Test Case: Verify Navigation to Defect Page ********");
+
+        try {
+            login();
+            logger.info("Logged in successfully and dashboard loaded");
+            DefectLandingPage defectLandingPage = new DefectLandingPage(getDriver());
+            defectLandingPage.clickDefectTab();
+            logger.info("Clicked on Defect Tab");
+            String actualUrl = getDriver().getCurrentUrl();
+            Assert.assertNotNull(actualUrl);
+            Assert.assertTrue(actualUrl.contains(expectedUrlAfterClick),
+                    "User did not navigate to the expected Defect Page URL.");
+            logger.info("Successfully navigated to Defect Page. Current URL: " + actualUrl);
+            logger.info("Defect Page loaded and form fields are visible.");
+            defectLandingPage.clickCreateTestCaseButton();
+            logger.info("clicked on Create Defect Button");
+
+            CreateDefectPage createDefectPage = new CreateDefectPage(getDriver());
+            createDefectPage.enterSummary(Summary);
+            logger.info("Summary filled:"+Summary);
+
+            createDefectPage.enterDescription(Description);
+            logger.info("Description filled:"+Description);
+
+            createDefectPage.verifyStatusIsDefault();
+            createDefectPage.clickSave();
+            logger.info("Clicked on save button");
+
+            createDefectPage.verifyStatusErrorMessage();
+            logger.info("Verified: Status mandatory error message displayed.");
+
+
+
+        } catch (AssertionError ae) {
+            logger.error("Assertion failed: " + ae.getMessage());
+            throw ae;
+        } catch (Exception ex) {
+            logger.error("Exception occurred: " + ex.getMessage());
+            throw ex;
+        }
+
+        logger.info("************ Test Case Finished: Verify Navigation to Defect Page *************");
+    }
+}
