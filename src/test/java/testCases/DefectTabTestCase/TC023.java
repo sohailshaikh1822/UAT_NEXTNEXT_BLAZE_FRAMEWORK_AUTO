50d990e0ce55b389ea4ec58a4e8a0c7422117d11
abhi-sn-31 <abhi_sn@pursuitsoftware.com>
2025-12-02 17:29:33 +0530

    Completed TC004 and TC023

---
+++ src/test/java/testCases/DefectTabTestCase/TC023.java
@@ -0,0 +1,64 @@
+package testCases.DefectTabTestCase;
+
+import DataProviders.DefectTabTestCaseDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.defectTab.CreateDefectPage;
+import pageObjects.defectTab.DefectLandingPage;
+import testBase.BaseClass;
+import utils.RetryAnalyzer;
+
+public class TC023 extends BaseClass {
+
+    @Test(dataProvider = "tc023", dataProviderClass = DefectTabTestCaseDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
+    public void nonManditoryFieldsLeftBlank(String project, String summary, String status, String description) throws InterruptedException {
+
+        logger.info("****** Starting Test Case: Verify non-mandatory fields can be left blank ********");
+
+        try {
+            // Step 1: Login
+            login();
+            logger.info("Logged in successfully and dashboard loaded");
+            Thread.sleep(10000);
+
+            // Step 2: Navigate to Defect tab
+            DefectLandingPage defectLandingPage = new DefectLandingPage(getDriver());
+            defectLandingPage.clickDefectTab();
+            logger.info("defect tab clicked");
+            Thread.sleep(10000);
+
+            // Step 3: Select the project and click on create defect button to open new defect form.
+            defectLandingPage.selectProject(project);
+            logger.info("project selected");
+            Thread.sleep(5000);
+
+            defectLandingPage.clickCreateTestCaseButton();
+            logger.info("Clicked on Create Test Case Button");
+
+            // Step 4: Fill the mandatory fields only and create the defect.
+            CreateDefectPage createDefectPage = new CreateDefectPage(getDriver());
+            createDefectPage.enterSummary(summary);
+            logger.info("Summary Entered");
+            createDefectPage.selectStatus(status);
+            logger.info("Defect status selected");
+            createDefectPage.enterDescription(description);
+            logger.info("Description Added");
+
+//            createDefectPage.scrollUp();
+            createDefectPage.clickSave();
+            logger.info("Save Button Clicked");
+            Assert.assertTrue(createDefectPage.verifySuccessMessage("Defect created successfully."), "Failed to create the defect.");
+            logger.info("New Defect Created");
+
+            logger.info("***** Defect created successfully without any on-mandatory fields. *******");
+        } catch (AssertionError ae) {
+            logger.error("Assertion failed: {}", ae.getMessage());
+            throw ae;
+        } catch (Exception ex) {
+            logger.error("Exception occurred: {}", ex.getMessage());
+            throw ex;
+        }
+
+    }
+
+}
