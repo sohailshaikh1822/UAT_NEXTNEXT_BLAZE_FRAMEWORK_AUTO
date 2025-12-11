22c8f6e3911261f807f9a8e84434cd96c6faa6e7
MohitAman <mohit_a@pursuitsoftware.com>
2025-12-11 13:15:10 +0530

    changes done in TC37 data and added wait

--- src/test/java/testCases/authorTabTestCase/TC037.java
+++ src/test/java/testCases/authorTabTestCase/TC037.java
@@ -10,7 +10,7 @@ import utils.WaitUtils;
 
 public class TC037 extends BaseClass {
 
-    @Test(dataProvider = "AddTest", dataProviderClass = AuthorTestCaseDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
+    @Test(dataProvider = "tc037", dataProviderClass = AuthorTestCaseDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
     public void verifyTestCaseCreation(
             String epic,
             String feature,
@@ -20,14 +20,19 @@ public class TC037 extends BaseClass {
         try {
             login();
             logger.info("Logged in successfully");
+            WaitUtils.waitFor1000Milliseconds();
             AuthorTestCasePage authorTestCasePage = new AuthorTestCasePage(getDriver());
             authorTestCasePage.clickAuthorTestcase();
+            WaitUtils.waitFor1000Milliseconds();
             logger.info("Clicked the Author test case");
             authorTestCasePage.selectEpic(epic);
+            WaitUtils.waitFor1000Milliseconds();
             logger.info("Selected the epic");
             authorTestCasePage.selectFeature(feature);
+            WaitUtils.waitFor1000Milliseconds();
             logger.info("Selected the feature");
             authorTestCasePage.clickRequirement(requirementId);
+            WaitUtils.waitFor1000Milliseconds();
             logger.info("Clicked on requirement id " + requirementId);
             WaitUtils.waitFor2000Milliseconds();;
             Assert.assertTrue(authorTestCasePage.isAllTestIdSorted());
