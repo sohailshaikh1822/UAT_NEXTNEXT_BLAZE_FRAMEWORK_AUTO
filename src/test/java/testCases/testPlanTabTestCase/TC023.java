c108794e32e5448e4362ca4155f6686baa0c77e0
VivekRajput <vivekrajput9575@gmail.com>
2026-01-09 19:15:48 +0530

    Added TC-21,22,23

---
+++ src/test/java/testCases/testPlanTabTestCase/TC023.java
@@ -0,0 +1,121 @@
+package testCases.testPlanTabTestCase;
+
+import DataProviders.TestPlanDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.requirementTab.AddRequirementPage;
+import pageObjects.requirementTab.RequirementTabPage;
+import pageObjects.testPlanTab.IndividualTestCyclePage;
+import pageObjects.testPlanTab.IndividualTestSuitePage;
+import pageObjects.testPlanTab.TestPlanLandingPage;
+import testBase.BaseClass;
+import utils.RetryAnalyzer;
+import utils.WaitUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TC023 extends BaseClass {
+    @Test(dataProvider = "tc010", dataProviderClass = TestPlanDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
+    public void verifyNotificationPopUpWhileCreationOfTestSuite(
+            String projectName,
+            String releaseName,
+            String testCycleName,
+            String testDescription,
+            String suiteName,
+            String startDate,
+            String endDate,
+            String executionType)
+            throws InterruptedException {
+        logger.info(
+                "****** Starting Test Case: Verify Release List Updates Based on Project Selection *****************");
+        try {
+
+            login();
+            logger.info("Logged in successfully");
+
+            TestPlanLandingPage testPlanPage = new TestPlanLandingPage(getDriver());
+
+            testPlanPage.selectTestPlanTab();
+            logger.info("Navigated to Test Plan tab");
+
+            WaitUtils.waitFor2000Milliseconds();
+            testPlanPage.expandSidebarIfCollapsed();
+            logger.info("Sidebar expanded if it was collapsed");
+
+            testPlanPage.clickOnTheProjectName();
+            logger.info("Selected project: " + projectName);
+            testPlanPage.clickOnReleaseOrTestCycleOrTestSuite(releaseName);
+            logger.info("Clicked on the module ");
+            WaitUtils.waitFor1000Milliseconds();
+
+            testPlanPage.clickNewTestCycle();
+            logger.info("Clicked on the new testCycle");
+
+            IndividualTestCyclePage individualTestCyclePage = new IndividualTestCyclePage(getDriver());
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+            String testCycleNameWithTimestamp = testCycleName + "_" + timestamp;
+            individualTestCyclePage.setTestCycleName(testCycleNameWithTimestamp);
+            logger.info("added the test cycle name");
+
+            individualTestCyclePage.setDescription(testDescription);
+            logger.info("added the description for cycle");
+
+            Assert.assertEquals(individualTestCyclePage.getTargetRelease(), releaseName);
+            logger.info("verified the targeted release ");
+
+            individualTestCyclePage.clickSave();
+            logger.info("Clicked on the save button");
+             WaitUtils.waitFor2000Milliseconds();
+            testPlanPage.clickOnReleaseOrTestCycleOrTestSuite(testCycleNameWithTimestamp);
+            logger.info("navigated to the created cycle");
+
+            testPlanPage.clickNewTestSuite();
+            logger.info("Clicked on the new test suite icon ");
+
+            IndividualTestSuitePage individualTestSuitePage = new IndividualTestSuitePage(getDriver());
+            RequirementTabPage requirementTabPage =new RequirementTabPage(getDriver());
+            AddRequirementPage addRequirementPage=new AddRequirementPage(getDriver());
+
+            String testSuiteNameWithTimestamp = suiteName + "_" + timestamp;
+            individualTestSuitePage.enterTestSuiteName(testSuiteNameWithTimestamp);
+            logger.info("Entered the suite name");
+
+            individualTestSuitePage.enterDescription(testDescription);
+            logger.info("entered the description");
+
+            individualTestSuitePage.setPlannedStartDate(startDate);
+            individualTestSuitePage.setPlannedEndDate(endDate);
+            logger.info("Entered the date");
+
+            individualTestSuitePage.selectExecutionType(executionType);
+            logger.info("selected the execution type");
+
+            individualTestSuitePage.clickSaveButton();
+            logger.info("clicked on save button");
+
+
+            String expectedNotificationPopup= requirementTabPage.handleToastNotification();
+            String tsId = addRequirementPage.getModuleId();
+            tsId = tsId.replaceAll("\\*$", "");
+            tsId = "'" + tsId.replace("'", "") + "'";
+
+            logger.info("new rl"+ tsId);
+
+            Assert.assertEquals(tsId +" is created by Julie Kumari.",expectedNotificationPopup,"not matched");
+
+
+
+
+
+        } catch (AssertionError e) {
+            logger.error("Assertion failed: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("Exception occurred: {}", e.getMessage());
+            throw e;
+        }
+
+        logger.info("************ Test Case Finished *************************");
+    }
+}
