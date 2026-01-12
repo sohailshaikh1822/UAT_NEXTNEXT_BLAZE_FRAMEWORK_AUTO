3cba926baea143dbbf037ed5517c92e4cb0bfb08
MohitAman <mohit_a@pursuitsoftware.com>
2026-01-12 16:56:36 +0530

    TC-60 added
    added & updated method
    
    selectStatus(String status)
    verifyTestRunUpdateNotification(String trId)
    
    in ExecuteLandingPage.Java

---
+++ src/test/java/testCases/ExecuteTestCaseTab/TC060.java
@@ -0,0 +1,99 @@
+package testCases.ExecuteTestCaseTab;
+
+import DataProviders.ExecuteTestCaseDataProvider;
+import org.testng.annotations.Test;
+import pageObjects.authoTestCaseTab.AuthorTestCasePage;
+import pageObjects.executeTestCaseTab.ExecuteLandingPage;
+import pageObjects.executeTestCaseTab.IndividualTestRun;
+import testBase.BaseClass;
+import utils.RetryAnalyzer;
+import utils.WaitUtils;
+
+public class TC060 extends BaseClass {
+
+    @Test(
+            dataProvider = "tc060",
+            dataProviderClass = ExecuteTestCaseDataProvider.class,
+            retryAnalyzer = RetryAnalyzer.class
+    )
+    public void Verify_popup_notification_appears_automatically_when_a_Test_Run_is_Updated(
+            String releaseName,
+            String cycleName,
+            String subcycleName,
+            String suiteName,
+            String Epicname,
+            String Featurename,
+            String rq,
+            String TC,
+            String Status
+    ) throws InterruptedException {
+
+        logger.info("****** Starting TC060: Verify Test Run update notification ******");
+
+        try {
+            login();
+            logger.info("Logged in successfully");
+
+            ExecuteLandingPage executeLandingPage = new ExecuteLandingPage(getDriver());
+            AuthorTestCasePage authorTestCasePage = new AuthorTestCasePage(getDriver());
+            IndividualTestRun individualTestRun = new IndividualTestRun(getDriver());
+
+            executeLandingPage.clickExecuteTab();
+
+            WaitUtils.waitFor1000Milliseconds();
+            executeLandingPage.clickArrowRightToExpandModule(releaseName);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickArrowRightToExpandModule(cycleName);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickArrowRightToExpandModule(subcycleName);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickOnSuite(suiteName);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickCreateTestRunButton();
+
+            WaitUtils.waitFor2000Milliseconds();
+            authorTestCasePage.selectEpic(Epicname);
+
+            WaitUtils.waitFor2000Milliseconds();
+            authorTestCasePage.selectFeature(Featurename);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickRequirementById(rq);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.selectTestCaseCheckbox(TC);
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickSaveInPopup();
+
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.openLatestTestRunFromTable();
+
+            String trId = executeLandingPage.getOpenedTestRunId();
+            logger.info("Opened Test Run ID: " + trId);
+
+            executeLandingPage.selectStatus(Status);
+            logger.info("Updated Test Run status to: " + Status);
+
+            WaitUtils.waitFor2000Milliseconds();
+            individualTestRun.clickSaveButton();
+            logger.info("Clicked Save button for Test Run update");
+
+            executeLandingPage.verifyTestRunUpdateNotification(trId);
+            logger.info("Test Run update notification verified successfully");
+
+        } catch (AssertionError e) {
+            logger.error("Assertion failed: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("Exception occurred: {}", e.getMessage());
+            throw e;
+        }
+
+        logger.info("************ Finished TC060 *************************");
+    }
+}
