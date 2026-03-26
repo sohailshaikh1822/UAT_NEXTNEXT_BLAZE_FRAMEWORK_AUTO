9168f1c3f56c4d66095e983a2cae472dbfac5c6d
Swarnadeep Mondal <swarnadeep_m@pursuitsoftware.biz>
2026-03-26 15:48:24 +0530

    Updated the old automation tasks

---
+++ src/test/java/testCases/ExecuteTestCaseTab/TC082.java
@@ -0,0 +1,71 @@
+package testCases.ExecuteTestCaseTab;
+
+import DataProviders.ExecuteTestCaseDataProvider;
+import org.testng.annotations.Test;
+import pageObjects.defectTab.DefectLandingPage;
+import pageObjects.executeTestCaseTab.ExecuteLandingPage;
+import pageObjects.executeTestCaseTab.IndividualTestRun;
+import testBase.BaseClass;
+import utils.ExportListener;
+import utils.NotificationsListener;
+import utils.RetryAnalyzer;
+import utils.WaitUtils;
+
+import javax.swing.text.DefaultEditorKit;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class TC082 extends BaseClass {
+
+    @Test(
+            dataProvider = "tc080",
+            dataProviderClass = ExecuteTestCaseDataProvider.class,
+            retryAnalyzer = RetryAnalyzer.class
+    )
+    public void Verify_CSV_export_downloads_successfully(
+            String releaseName,
+            String trId ,
+            String defectId
+    ) throws InterruptedException
+    {
+        logger.info("*************** Starting TC082 *****************");
+        try {
+            login();
+            logger.info("Logged in successfully");
+            ExecuteLandingPage executeLandingPage = new ExecuteLandingPage(getDriver());
+            NotificationsListener notificationsListener=new NotificationsListener(getDriver());
+            WaitUtils.waitFor3000Milliseconds();
+            executeLandingPage.clickExecuteTab();
+            logger.info("Clicked Execute Test Case tab");
+            WaitUtils.waitFor1000Milliseconds();
+            executeLandingPage.clickRelease(releaseName);
+            logger.info("Selected release: " + releaseName);
+            WaitUtils.waitFor2000Milliseconds();
+            executeLandingPage.clickTestRunById(trId);
+            logger.info("Clicked Selected Id");
+            DefectLandingPage defectLandingPage = new DefectLandingPage(getDriver());
+            WaitUtils.waitFor3000Milliseconds();
+            IndividualTestRun individualTestRun = new IndividualTestRun(getDriver());
+            WaitUtils.waitFor1000Milliseconds();
+            individualTestRun.verifyExportButtonOfTrVisibleAndClickable();
+            ExportListener exportListener = new ExportListener(getDriver());
+
+            WaitUtils.waitFor3000Milliseconds();
+            exportListener.selectCsvFileType();
+            WaitUtils.waitFor3000Milliseconds();
+            defectLandingPage.clickSaveExportButton();
+
+            exportListener.pressSaveButton();
+
+            WaitUtils.waitFor3000Milliseconds();
+            logger.info("Successfully verified the excel column name");
+        } catch (AssertionError e) {
+            logger.error("Assertion failed: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("Exception occurred: {}", e.getMessage());
+        }
+        logger.info("************ TC082 Finished ***************");
+    }
+}
